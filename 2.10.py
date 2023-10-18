653a50ca82bac570b5d7474267085f44a881c18f
Michal <micadawp.pl@wp.pl>
2023-10-18 19:18:11 +0200

    firsCommit

---
+++ 2.10.py
@@ -0,0 +1,6 @@
+line = """przykladowy tekst     kocham  
+pythona elegancko"""
+
+words = len(line.split())
+
+print("Number of words:", words)
---
+++ 2.11.py
@@ -0,0 +1,9 @@
+line = "word"
+word = ""
+
+for i in line:
+    word += i + "_"
+
+word = word[:-1]
+
+print(word)
